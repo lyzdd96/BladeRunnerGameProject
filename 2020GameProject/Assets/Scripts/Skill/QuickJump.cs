6fb0a2a6698bd3f223ba3a6196c33e80df9c0422
ssea <jian.ca@hotmail.com>
2020-12-28 02:22:00 -0500

    direction quickjump

--- 2020GameProject/Assets/Scripts/Skill/QuickJump.cs
+++ 2020GameProject/Assets/Scripts/Skill/QuickJump.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class QuickJump : Skill {
+    // which Character object to apply the skill
     Character target;
     public QuickJump(Attack attack, float cooldown, Character target): base(attack, cooldown) {
         this.target = target;
@@ -10,11 +11,15 @@ public class QuickJump : Skill {
     }
 
     public override void runSkill() {
-        float deltaAngle = 0f;
+        float verticalMove = Input.GetAxisRaw("Vertical") * 5;
+        float deltaAngle = verticalMove;
         float backJumpForce = 4000f;
+        bool isJumping = ((Player)target).isJumping;
+        
         Vector2 direction = new Vector2(Mathf.Cos(Mathf.Deg2Rad*deltaAngle) * backJumpForce, Mathf.Sin(Mathf.Deg2Rad*deltaAngle) * backJumpForce);
-        if (this.target.isFacingRight)
+        if (!this.target.isFacingRight)
             direction.x = direction.x * -1;
+        
         target.thisRB.AddForce(direction);
     }
 }
