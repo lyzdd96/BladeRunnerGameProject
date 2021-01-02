03e81ae9315689d2f47dbd6b56fe9c121a21d2ad
ZiQi <ziqi.li@mail.mcgill.ca>
2021-01-01 22:50:08 -0500

    fix monster AI and add wander feature

--- 2020GameProject/Assets/Scripts/BehaviourTreeAI/FlyingMonsterAI.cs
+++ 2020GameProject/Assets/Scripts/BehaviourTreeAI/FlyingMonsterAI.cs
@@ -25,6 +25,7 @@ public class FlyingMonsterAI : MonoBehaviour
         // get the player gameObject from the game flow manager
         player = GameObject.Find("GameManager").GetComponent<GameFlowManager>().getPlayer();
 
+        /*
         // build the behaviour tree
         _tree = new BehaviorTreeBuilder(gameObject)
             .Selector()
@@ -41,7 +42,7 @@ public class FlyingMonsterAI : MonoBehaviour
                         .Do("PathFinding to Player", () => {
                             // perform path finding to the player
                             movementController.pathFinding(player.transform.position);
-                            return TaskStatus.Failure;  // return failure to keep path finding if needed
+                            return TaskStatus.Success;  // return failure to keep path finding if needed
                         })
                     .End()
                     .WaitTime(1f)  // wait 3 seconds before attack
@@ -55,9 +56,41 @@ public class FlyingMonsterAI : MonoBehaviour
                     return TaskStatus.Success;
                 })
             .End()
+            .Build();*/
+
+        // build the behaviour tree
+        _tree = new BehaviorTreeBuilder(gameObject)
+            .Selector()
+                .Sequence()
+                    .Condition("isPlayerInAttackRange", () => {
+                        // check the distance between this monster and the player
+                        return Vector3.Distance(this.transform.position, player.transform.position) <= attackRange;
+                    })
+                    .WaitTime(1f)  // wait 1 second before each attack action
+                    .Do("Attack", () => {
+                        attackController.attack(player, 0.25f, 3);
+                        return TaskStatus.Success;
+                    })
+                .End()
+                .Sequence()
+                    .Condition("isPlayerInAlertRange", () => {
+                        // check the distance between this monster and the player
+                        return Vector3.Distance(this.transform.position, player.transform.position) <= alertRange;
+                    })
+                    .Do("PathFinding to Player", () => {
+                        // perform path finding to the player
+                        movementController.pathFinding(player.transform.position);
+                        return TaskStatus.Success;  // return failure to keep path finding if needed
+                    })
+                .End()
+                .Do("Wandering", () => {
+                    movementController.wander();  // perform wander action
+                    return TaskStatus.Success;
+                })
+            .End()
             .Build();
 
-        
+
     }
 
     // Update is called once per frame
@@ -65,5 +98,6 @@ public class FlyingMonsterAI : MonoBehaviour
     {
         // Update our tree every frame
         _tree.Tick();
+        Debug.Log(Vector3.Distance(player.transform.position, this.transform.position));
     }
 }
